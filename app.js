14afca1ab15ec71a0566daee8f5a8b6a251ebe00
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-10-07 12:06:56 +0530

    add app js file

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const user = require('./routes/user')
+const posts = require('./routes/posts')
+
+const app=express()
+const port = 4002
+
+const url = 'mongodb://127.0.0.1/carSellMobileApp'
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on("open", () => {
+    console.log('MongoDB connected!');
+})
+
+app.use(express.json())
+
+app.use('/user', user)
+app.use('/posts', posts)
+
+app.listen(port, () => {
+    console.log(`app starting on ${port}`);
+})
