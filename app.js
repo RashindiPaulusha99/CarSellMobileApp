22e608358dd531e80740f383a08cba1f1041af5d
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-10-07 12:21:11 +0530

    add routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const user = require('./routes/user')
+const posts = require('./routes/posts')
 
 const app=express()
 const port = 4002
@@ -17,6 +18,7 @@ con.on("open", () => {
 app.use(express.json())
 
 app.use('/user', user)
+app.use('/posts', posts)
 
 app.listen(port, () => {
     console.log(`app starting on ${port}`);
